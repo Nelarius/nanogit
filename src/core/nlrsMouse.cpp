7e1fd656a257e4239063c5624cb35c06293809b6
Johann Muszynski <johann.muszynski@gmail.com>
2017-02-13 20:23:33 +0200

    Handle mouse button and scroll events

--- src/core/nlrsMouse.cpp
+++ src/core/nlrsMouse.cpp
@@ -12,20 +12,49 @@ Vec2i Mouse::currentCoordinates() const
     return coords;
 }
 
+void Mouse::listenToButtonDown(ButtonCallback callback)
+{
+    buttonDownSignal_.connect(callback);
+}
+
+void Mouse::listenToButtonUp(ButtonCallback callback)
+{
+    buttonUpSignal_.connect(callback);
+}
+
+void Mouse::listenToScroll(ScrollCallback callback)
+{
+    scrollSignal_.connect(callback);
+}
+
 bool Mouse::handleEvent(const SDL_Event& event)
 {
+    static_assert(static_cast<int>(Button::Left) == SDL_BUTTON_LEFT, "Button::Left not equal to SDL_BUTTON_LEFT");
+    static_assert(static_cast<int>(Button::Middle) == SDL_BUTTON_MIDDLE, "Button::Middle not equal to SDL_BUTTON_MIDDLE");
+    static_assert(static_cast<int>(Button::Right) == SDL_BUTTON_RIGHT, "Button::Right not equal to SDL_BUTTON_RIGHT");
+
     if (event.type == SDL_MOUSEBUTTONDOWN)
     {
-        // TODO:
+        buttonDownSignal_.emit(static_cast<Button>(event.button.button), coordinates_);
+        return true;
+    }
+    else if (event.type == SDL_MOUSEBUTTONUP)
+    {
+        buttonUpSignal_.emit(static_cast<Button>(event.button.button), coordinates_);
+        return true;
+    }
+    else if (event.type == SDL_MOUSEWHEEL)
+    {
+        scrollSignal_.emit(event.wheel.y, coordinates_);
         return true;
     }
 
-    if (event.type == SDL_MOUSEBUTTONUP)
-    {
-        // TODO:
-        return true;
-    }
     return false;
+}
+
+void Mouse::update()
+{
+    SDL_GetMouseState(&coordinates_.x, &coordinates_.y);
 }
 
 }
--- src/core/nlrsMouse.h
+++ src/core/nlrsMouse.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include "nlrsArray.h"
 #include "nlrsLocator.h"
 #include "nlrsVector.h"
+#include "SimpleSignal.h"
 
 #include <functional>
 
@@ -18,7 +20,7 @@ public:
     * defined in http://hg.libsdl.org/SDL/file/default/include/SDL_mouse.h
     */
     enum class Button : int {
-        Left = 1,
+        Left = 1u,
         Middle,
         Right
     };
@@ -29,13 +31,48 @@ public:
         ButtonUp
     };
 
-    using Callback = std::function<void(Button button)>;
+    // The callback gets called with the event button, and the event coordinates
+    using ButtonCallback = std::function<void(Button, Vec2i)>;
+    // The callback gets called the direction of the scroll, as well as the event coordinates.
+    // A positive delta indicates scrolling away from the user, and a negative delta
+    // scrolling towards the user.
+    using ScrollCallback = std::function<void(i32, Vec2i)>;
+
+    Mouse()
+        : mouseState_(),
+        coordinates_(0, 0),
+        buttonDownSignal_(),
+        buttonUpSignal_(),
+        scrollSignal_()
+    {
+        mouseState_.pushBack(ButtonState{ Button::Left, Event::ButtonUp });
+        mouseState_.pushBack(ButtonState{ Button::Middle, Event::ButtonUp });
+        mouseState_.pushBack(ButtonState{ Button::Right, Event::ButtonUp });
+    }
 
     Vec2i currentCoordinates() const;
+
+    void listenToButtonDown(ButtonCallback callback);
+    void listenToButtonUp(ButtonCallback callback);
+    void listenToScroll(ScrollCallback callback);
+
+    void update();
 
     bool handleEvent(const SDL_Event& event);
 
 private:
+    struct ButtonState
+    {
+        Button button;
+        Event status;
+    };
+
+    StaticArray<ButtonState, 3> mouseState_;
+    Vec2i coordinates_;
+
+    Simple::Signal<void(Button, Vec2i)> buttonDownSignal_;
+    Simple::Signal<void(Button, Vec2i)> buttonUpSignal_;
+    Simple::Signal<void(i32, Vec2i)> scrollSignal_;
 };
 
 using MouseLocator = Locator<Mouse>;
